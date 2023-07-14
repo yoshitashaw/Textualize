452397be900e2431359a851f98da629f5a3d98f4
yoshitashaw <109474278+yoshitashaw@users.noreply.github.com>
2023-07-14 14:15:19 +0000

    Add files via upload

---
+++ scr/components/Textform.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+
+export default function TextForm (props){
+    //button to convert into Uppercase
+    const handleUpClick= () =>{
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    
+    //button to convert into Lowercase
+    const handleLoClick= () =>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    //button to convert into Reverse
+    const handleReverse= () =>{
+        let newSplit = text.split("")
+        let newRev = newSplit.reverse()
+        let newText = newRev.join("");
+        setText(newText)
+    }
+
+    //enables to write in the textbox
+    const handleOnChange = (event)=>{
+        setText(event.target.value)
+    }
+
+    const[text, setText]= useState("");
+    return(
+        <>
+        <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
+            <h1> {props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value={text} onChange ={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='light'?'black':'white'}} id="myBox" rows="8"> </textarea>
+            </div>
+            <button className="btn-btn-primary mx-1 btn btn-outline-warning" onClick= {handleUpClick}> Convert to UpperCase </button>
+            <button className="btn-btn-primary mx-1 btn btn-outline-warning" onClick= {handleLoClick}> Convert to LowerCase </button>
+            <button className="btn-btn-primary mx-1 btn btn-outline-warning" onClick= {handleReverse}> Convert to Reverse </button>
+        </div>
+
+        <div className="container my-3" style={{color: props.mode==='light'?'black':'white'}}>
+          <h2>Your Text Summary</h2>
+          <p>{text.split(" ").length} words and {text.length} characters.</p>
+          <p>{0.008* (text.split(" ")).length} Minutes read</p>
+          <h2>Preview</h2>
+          <p>{text.length>0? text: "Enter somethong in the textbox to preview"}</p>
+        </div>
+        </>
+    )
+}
